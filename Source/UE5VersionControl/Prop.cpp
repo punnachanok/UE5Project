bfca428178475c093e15bd4f2cc6ac8a690e241b
Punnachanok <punnachanok@gmail.com>
2022-05-27 16:33:46 +0700

    Created Prop Blueprint

--- Source/UE5VersionControl/Prop.cpp
+++ Source/UE5VersionControl/Prop.cpp
@@ -1,21 +1,56 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+//#include "Prop.h"
+//
+//// Sets default values
+//AProp::AProp()
+//{
+// 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+//	PrimaryActorTick.bCanEverTick = true;
+//
+//	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Comp"));
+//	RootComponent = MeshComp;
+//
+//
+//}
+//
+//// Called when the game starts or when spawned
+//void AProp::BeginPlay()
+//{
+//	Super::BeginPlay();
+//	
+//}
+//
+//// Called every frame
+//void AProp::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+//
+
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "Prop.h"
 
 // Sets default values
 AProp::AProp()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Comp"));
+	RootComponent = MeshComp;
 }
 
 // Called when the game starts or when spawned
 void AProp::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -24,4 +59,3 @@ void AProp::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
